2a9a217e3a17377fb57f7a8ffa8502f4f57913eb
dika31 <34057475+dika31@users.noreply.github.com>
2017-12-29 10:30:00 +0700

    Update pemanggil.js

--- pemanggil.js
+++ pemanggil.js
@@ -19,11 +19,18 @@ var a_to_ve="";var a_to_vf=new Array();var a_to_vg=0;a_to_ve=document.getElement
 j++;}
 if(a_to_vh==false)
 {var encryptedUrl=Base64.encode(a_to_vi);
-var x=Math.floor((Math.random()*3)+ 1);
+var x=Math.floor((Math.random()*10)+ 1);
 var xxx=null;
 if(x=="1"){xxx="/2017/12/mengenal-spongebob-squarepants.html"}
 if(x=="2"){xxx="/2017/12/mengenal-gravity-falls.html"}
-if(x=="3"){xxx="/2017/12/mengenal-steven-universe.html"}a_to_ve[i].href="https://saflink31.blogspot.com/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="3"){xxx="/2017/12/mengenal-steven-universe.html"}
+if(x=="4"){xxx="/2017/12/mengenal-avatar-legend-of-aang.html"}
+if(x=="5"){xxx="/2017/12/mengenal-regular-show.html"}
+if(x=="6"){xxx="/2017/12/mengenal-amazing-world-of-gumball.html"}
+if(x=="7"){xxx="/2017/12/mengenal-doraemon.html"}
+if(x=="8"){xxx="/2017/12/mengenal-film-wall-e.html"}
+if(x=="9"){xxx="/2017/12/mengenal-film-spongebob-squarepants.html"}
+if(x=="10"){xxx="/2017/12/mengenal-film-toy-story.html"}a_to_ve[i].href="https://saflink31.blogspot.com/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
